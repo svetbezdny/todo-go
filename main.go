2358edc422fb6110dd13012e81fc1231e7cf2659
svetbezdny <kostikov-34@mail.ru>
2025-06-18 22:38:18 +0300

    add log middleware

--- main.go
+++ main.go
@@ -18,10 +18,14 @@ var (
 
 func main() {
 
-	http.HandleFunc("/todo", TodoHandler)
-	http.HandleFunc("/todo/", TodoByIdHandler)
+	mux := http.NewServeMux()
 
-	err := http.ListenAndServe(":3000", nil)
+	mux.HandleFunc("/todo", TodoHandler)
+	mux.HandleFunc("/todo/", TodoByIdHandler)
+
+	handler := LogMiddleware(mux)
+
+	err := http.ListenAndServe(":3000", handler)
 	if err != nil {
 		panic(err)
 	}
@@ -33,19 +37,16 @@ func TodoHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 
 	case http.MethodGet:
-		log.Println(Log("GET todo/"))
 		json.NewEncoder(w).Encode(TodoList)
 
 	case http.MethodPost:
 		var newTodo TodoItem
 		if err := json.NewDecoder(r.Body).Decode(&newTodo); err != nil {
-			log.Println(Log("POST todo/ Invalid item format").WithLevel(LevelError))
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
 		if newTodo.Item == "" {
-			log.Println(Log("POST todo/<todo_id> Item cannot be empty").WithLevel(LevelWarning))
 			http.Error(w, "Item cannot be empty", http.StatusBadRequest)
 			return
 		}
@@ -55,15 +56,12 @@ func TodoHandler(w http.ResponseWriter, r *http.Request) {
 		mu.Lock()
 		TodoList = append(TodoList, todo)
 		mu.Unlock()
-		log.Println(Log("POST todo/"))
 		w.WriteHeader(http.StatusCreated)
 		json.NewEncoder(w).Encode(todo)
 	case http.MethodDelete:
-		log.Println(Log("DELETE todo/"))
 		TodoList = []Todo{}
 		json.NewEncoder(w).Encode(map[string]string{"message": "Todo cleared successfully"})
 	default:
-		log.Println(Log("todo/ Method not allowed").WithLevel(LevelWarning))
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
@@ -75,7 +73,6 @@ func TodoByIdHandler(w http.ResponseWriter, r *http.Request) {
 	todoId := strings.TrimPrefix(r.URL.Path, "/todo/")
 	id, err := strconv.Atoi(todoId)
 	if err != nil {
-		log.Println(Log("todo/<todo_id> Invalid todo id").WithLevel(LevelError))
 		http.Error(w, "Invalid todo id", http.StatusBadRequest)
 		return
 	}
@@ -89,31 +86,26 @@ func TodoByIdHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if todo == nil {
-		log.Println(Log("todo/<todo_id> Todo not found").WithLevel(LevelWarning))
 		http.Error(w, fmt.Sprintf("Todo with id %d not found", id), http.StatusNotFound)
 		return
 	}
 
 	switch r.Method {
 	case http.MethodGet:
-		log.Println(Log("GET todo/<todo_id>"))
 		json.NewEncoder(w).Encode(todo)
 	case http.MethodPut:
 		var updatedTodo TodoItem
 		if err := json.NewDecoder(r.Body).Decode(&updatedTodo); err != nil {
-			log.Println(Log("PUT todo/{todo_id} Invalid item format").WithLevel(LevelError))
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
 		if updatedTodo.Item == "" {
-			log.Println(Log("PUT todo/{todo_id} Item cannot be empty").WithLevel(LevelWarning))
 			http.Error(w, "Item cannot be empty", http.StatusBadRequest)
 			return
 		}
 
 		todo.Item = updatedTodo.Item
-		log.Println(Log("PUT todo/{todo_id}"))
 		json.NewEncoder(w).Encode(todo)
 	case http.MethodDelete:
 		for idx := range TodoList {
@@ -121,13 +113,19 @@ func TodoByIdHandler(w http.ResponseWriter, r *http.Request) {
 				mu.Lock()
 				TodoList = append(TodoList[:idx], TodoList[idx+1:]...)
 				mu.Unlock()
-				log.Println(Log("DELETE todo/<todo_id>"))
 				json.NewEncoder(w).Encode(map[string]string{"message": "Todo deleted successfully"})
 				return
 			}
 		}
 	default:
-		log.Println(Log("todo/<todo_id> Method not allowed").WithLevel(LevelWarning))
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
+}
+
+func LogMiddleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mw := &LogResponseWriter{ResponseWriter: w, StatusCode: http.StatusOK}
+		handler.ServeHTTP(mw, r)
+		log.Printf("%s [%d]", r.URL.RequestURI(), mw.StatusCode)
+	})
 }
--- struct.go
+++ struct.go
@@ -1,9 +1,23 @@
 package main
 
+import (
+	"net/http"
+)
+
 type Todo struct {
 	ID   int    `json:"id"`
 	Item string `json:"item"`
 }
 type TodoItem struct {
 	Item string `json:"item"`
+}
+
+type LogResponseWriter struct {
+	http.ResponseWriter
+	StatusCode int
+}
+
+func (w *LogResponseWriter) WriteHeader(StatusCode int) {
+	w.ResponseWriter.WriteHeader(StatusCode)
+	w.StatusCode = StatusCode
 }
